543fe0816884330adbcb26d07eb91b8dfa944b11
facilestapps-creator <facilest.apps@gmail.com>
2026-08-21 00:01:22 -0300

    Update health.ts

--- artifacts/api-server/src/routes/health.ts
+++ artifacts/api-server/src/routes/health.ts
@@ -7,5 +7,5 @@ router.get("/healthz", (_req, res) => {
   const data = HealthCheckResponse.parse({ status: "ok-v2" });
   res.json(data);
 });
-
+router.get("/test/foo", (_req, res) => { res.json({ ping: "pong" }); });
 export default router;
